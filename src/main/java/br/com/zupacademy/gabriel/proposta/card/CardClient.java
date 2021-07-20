68c4b6313d0439aab86e375a3c7cbc2b49fd215f
Gabriel <diasgabrieldg@gmail.com>
2021-07-20 16:44:40 -0300

    Notificando sistema legado do bloqueio do cartão

--- src/main/java/br/com/zupacademy/gabriel/proposta/card/CardClient.java
+++ src/main/java/br/com/zupacademy/gabriel/proposta/card/CardClient.java
@@ -1,8 +1,13 @@
 package br.com.zupacademy.gabriel.proposta.card;
 
+import java.util.Map;
+
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import feign.Response;
 
 @FeignClient(name = "card", url = "${api.cards.url}")
 public interface CardClient {
@@ -10,4 +15,6 @@ public interface CardClient {
 	@PostMapping
 	public CardResponse associateCard (@RequestBody CardRequest request);
 
+	@PostMapping("/{cardNumber}/bloqueios")
+	public Response notifyCardBlock(@PathVariable String cardNumber, @RequestBody Map<String, String> request);
 }
--- src/main/java/br/com/zupacademy/gabriel/proposta/card/block/BlockCardController.java
+++ src/main/java/br/com/zupacademy/gabriel/proposta/card/block/BlockCardController.java
@@ -1,8 +1,10 @@
 package br.com.zupacademy.gabriel.proposta.card.block;
 
+import java.util.Map;
 import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,7 +15,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.zupacademy.gabriel.proposta.card.Card;
+import br.com.zupacademy.gabriel.proposta.card.CardClient;
 import br.com.zupacademy.gabriel.proposta.card.CardRepository;
+import feign.Response;
 
 @RestController
 @RequestMapping("/cards")
@@ -21,7 +25,10 @@ public class BlockCardController {
 	
 	@Autowired
 	private CardRepository cardRepository;
+	@Autowired
+	private CardClient cardClient;
 	
+	@Transactional
 	@PostMapping("/{cardNumber}/blocks")
 	public ResponseEntity<?> blockCard (@PathVariable String cardNumber, HttpServletRequest request) {
 		Optional<Card> card = cardRepository.findById(cardNumber);
@@ -31,8 +38,14 @@ public class BlockCardController {
 		if(card.get().isBlocked()) {
 			return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body("Cartão de número " + cardNumber + " já está bloqueado.");
 		}
-		card.get().BlockCard(request);
-		cardRepository.save(card.get());
-		return ResponseEntity.ok().build();
+		
+		Response resp = cardClient.notifyCardBlock(cardNumber, Map.of("sistemaResponsavel:", "Proposal Microservice - Gabriel"));
+			if (resp.status() == 200) {
+				System.out.println(resp.body());
+				card.get().BlockCard(request);
+				cardRepository.save(card.get());
+				return ResponseEntity.ok().build();
+		}
+			return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body("Não foi possível bloquear o cartão no momento. Tente mais tarde!");
 	}
 }
